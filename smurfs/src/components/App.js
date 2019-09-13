670d09263e51bd618ea4da69208480d89ef78f96
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-13 09:34:17 -0600

    context set to smurf data api

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,37 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
-class App extends Component {
-  render() {
+
+//contexts
+import { SmurfContext } from '../contexts/SmurfContext';
+
+const App = () => {
+  const [smurfs, setSmurfs] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3333/smurfs`, {
+        params: {}
+      })
+      .then(response => {
+        const smurf = response.data;
+        console.log('smurf:', smurf);
+        setSmurfs(smurfs);
+      });
+  }, []);
+
+      
+
     return (
       <div className="App">
+        <SmurfContext.Provider value = { smurfs }>
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        </SmurfContext.Provider>
       </div>
     );
   }
-}
+
 
 export default App;
